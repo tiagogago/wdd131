7765fb9c31489eb17d7be16539bef4d2ae9b596e
Tiago Gago <138620862+tiagogago@users.noreply.github.com>
2024-11-06 16:40:49 +0100

    commit4

--- atv3/script/app.js
+++ atv3/script/app.js
@@ -1,25 +1,25 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const daysOfWeek = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
-//   const today = new Date();
-//   const todayIndex = today.getDay();
-//   const todayName = daysOfWeek[todayIndex];
+document.addEventListener("DOMContentLoaded", function () {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date();
+  const todayIndex = today.getDay();
+  const todayName = daysOfWeek[todayIndex];
 
-//   document.getElementById("today").innerText = `Today is ${todayName}.`;
+  document.getElementById("today").innerText = `Today is ${todayName}.`;
 
-//   const futureDaysList = document.getElementById("future-days-list");
-//   for (let i = 1; i <= 6; i++) {
-//     const futureDayIndex = (todayIndex + i) % 7;
-//     const futureDayName = daysOfWeek[futureDayIndex];
-//     const listItem = document.createElement("li");
-//     listItem.innerText = futureDayName;
-//     futureDaysList.appendChild(listItem);
-//   }
-// });
+  const futureDaysList = document.getElementById("future-days-list");
+  for (let i = 1; i <= 6; i++) {
+    const futureDayIndex = (todayIndex + i) % 7;
+    const futureDayName = daysOfWeek[futureDayIndex];
+    const listItem = document.createElement("li");
+    listItem.innerText = futureDayName;
+    futureDaysList.appendChild(listItem);
+  }
+});
