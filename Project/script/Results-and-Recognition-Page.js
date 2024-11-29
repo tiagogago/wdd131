3a74f93b6558839801740d7933ccba80e11967af
Tiago Gago <138620862+tiagogago@users.noreply.github.com>
2024-11-29 17:44:16 +0100

    final change

--- Project/script/Results-and-Recognition-Page.js
+++ Project/script/Results-and-Recognition-Page.js
@@ -7,9 +7,10 @@ var workSummaryChart = new Chart(ctx, {
       {
         label: "Last 9 Months",
         data: [500, 600, 800, 1200, 1500, 1300, 1100, 900, 1000],
-        borderColor: "#00f",
+        borderColor: "#ff7f32",
         backgroundColor: "rgba(0, 0, 255, 0.1)",
         fill: true,
+        backgroundColor:"#ff7f32",
       },
     ],
   },
@@ -21,3 +22,37 @@ var workSummaryChart = new Chart(ctx, {
     },
   },
 });
+
+function toggleAnswer(answer) {
+  if (answer.style.display == "") {
+    answer.style.display = "block";
+  } else {
+    answer.style.display = "";
+  }
+}
+
+document.querySelectorAll(".faq-question").forEach((button) => {
+  button.addEventListener("click", function (e) {
+    document.querySelectorAll(".faq-question").forEach((btn) => {
+      const answerElement = btn.parentElement.nextElementSibling;
+      answerElement.style.display = "";
+    });
+    const answerElement = button.parentElement.nextElementSibling;
+    toggleAnswer(answerElement);
+  });
+});
+
+function toggleMenu() {
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active"); // Adiciona ou remove a classe active
+}
+
+// Detecta mudanças no modo fullscreen
+document.addEventListener("fullscreenchange", () => {
+  if (document.fullscreenElement) {
+    document.body.classList.add("fullscreen"); // Adiciona a classe 'fullscreen' ao body
+  } else {
+    document.body.classList.remove("fullscreen"); // Remove a classe 'fullscreen' do body
+  }
+});
+
